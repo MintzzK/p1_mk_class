f0ae6836321e8292fd25bd64888de11fcf129b85
Mariyam Khan <mariyamk.121673@gmail.com>
2026-09-11 20:36:48 +0530

    Delete database directory

--- database/config.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from supabase import create_client, client
-# supabase ka ek instant ban gaya ab is se querie run kar sakte hai
-supabase: client= create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
