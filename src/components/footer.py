4f0f40ea73dbd3f359d4f7764c48d800790a5c87
Mariyam Khan <mariyamk.121673@gmail.com>
2026-09-19 13:27:10 +0530

    Add footer functions with logo and styling

--- src/components/footer.py
+++ src/components/footer.py
@@ -17,3 +17,40 @@ def footer_home(): #footer logo, footer
             
         </div>
     """, unsafe_allow_html=True)
+
+def footer_tr(): #footer logo, footer
+        logo_url = "https://i.pinimg.com/1200x/04/c2/32/04c23259de100bce953530beed503a96.jpg"
+    
+        st.markdown(f"""
+        <div style="margin-top:2rem;
+                    display:flex;
+                    gap:6px;
+                    align-item:center;
+                    justify-content:center">
+                 
+        <p style="font-weight:bold; color:black;"> 
+                Created with ❤️ by  
+                <img src="{logo_url}" style="max-height:25px;" />
+        </p>
+            
+        </div>
+    """, unsafe_allow_html=True)
+        
+
+def footer_st(): #footer logo, footer
+        logo_url = "https://i.pinimg.com/1200x/04/c2/32/04c23259de100bce953530beed503a96.jpg"
+    
+        st.markdown(f"""
+        <div style="margin-top:2rem;
+                    display:flex;
+                    gap:6px;
+                    align-item:center;
+                    justify-content:center">
+                 
+        <p style="font-weight:bold; color:black;"> 
+                Created with ❤️ by  
+                <img src="{logo_url}" style="max-height:25px;" />
+        </p>
+            
+        </div>
+    """, unsafe_allow_html=True)
