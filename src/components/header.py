ca8f5d582e3aa9e80668f2a96f780eb8043b3c92
Mariyam Khan <mariyamk.121673@gmail.com>
2026-09-19 13:27:44 +0530

    Refactor header functions and update HTML structure
    Refactor header functions to improve structure and styling.

--- src/components/header.py
+++ src/components/header.py
@@ -1,18 +1,63 @@
 import streamlit as st
 
+import streamlit as st
+
 def header_home():
     logo_url = "https://i.pinimg.com/736x/82/92/d7/8292d7783cec70bd9e0671f9230eb1c0.jpg"
 
-    st.markdown(f"""
-            <div style = "display:flex;
-                      flex-direction:column;
-                      align-items:center;
-                      justify-content:center; 
-                      margin-bottom:30px ; 
-                      margin-top:30px">       
-                   
+    st.markdown(
+        f"""
+        <div style="
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+            margin-top:30px;
+            margin-bottom:30px;
+        ">
             <img src="{logo_url}" style="height:100px;" />
-            <h1 style='text-align:center; color:#CFECEC'>M K<br/> CLASS </h1>
-            </div>
-""", unsafe_allow_html=True)
+            <h1 style="
+                text-align:center;
+                color:#CFECEC;
+                margin-top:10px;
+            ">
+                M K<br/>CLASS
+            </h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
     
+
+import streamlit as st
+def header_tr():
+    logo_url = "https://i.pinimg.com/736x/c0/1c/43/c01c43422cde952dd3ced914dfb14aba.jpg"
+
+    st.markdown(f"""
+        <div style = "
+                    display:flex;
+                    align-items:center;
+                    justify-content:center; 
+                    gap:10px">   
+                <img src="{logo_url}" style="height: 85px; border-radius: 10px;"/>
+                 <h2 style="text-align:left; color:#CFECEC">M K<br/>CLASS</h1>
+                
+         </div>
+         """,unsafe_allow_html=True) 
+
+    
+import streamlit as st
+def header_st():
+    logo_url = "https://i.pinimg.com/736x/c0/1c/43/c01c43422cde952dd3ced914dfb14aba.jpg"
+
+    st.markdown(f"""
+        <div style = "
+                    display:flex;
+                    align-items:center;
+                    justify-content:center; 
+                    gap:10px">   
+                <img src="{logo_url}" style="height: 85px; border-radius: 10px;"/>
+                 <h2 style="text-align:left; color:#CFECEC">M K<br/>CLASS</h1>
+                
+         </div>
+         """,unsafe_allow_html=True) 
